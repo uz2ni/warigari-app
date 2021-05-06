5e72e30f4d4fb2abe9bba69a935aeee7f174301c
uz2ni <uzini_@likelion.org>
2021-05-06 23:14:00 +0900

    [메인] 메인 탭 생성

--- app/app.js
+++ app/app.js
@@ -1,7 +1,27 @@
-import Vue from 'nativescript-vue'
+const Vue = require('nativescript-vue');
 
-import Home from './components/Home'
+const Master = {
+  template: `
+    <TabView :selectedIndex="selectedIndex">
+      <TabViewItem title="HOME">
+        <label text="홈, 베스트, 신상"></label>
+      </TabViewItem>
+      <TabViewItem title="SHOP">
+        <label text="랭킹, 즐겨찾기"></label>
+      </TabViewItem>
+      <TabViewItem title="ALL">
+        <label text="모아보기"></label>
+      </TabViewItem>
+      <TabViewItem title="HEART">
+        <label text="찜한 아이템"></label>
+      </TabViewItem>
+      <TabViewItem title="MY">
+        <label text="마이페이지"></label>
+      </TabViewItem>
+    </TabView>
+  `
+};
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
+  render: h => h(Master)
 }).$start()
