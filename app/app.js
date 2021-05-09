525036c564dae982b52344a4c2ef7352e4d64212
uz2ni <uzini_@likelion.org>
2021-05-09 11:07:01 +0900

    [메인] app.js 미해결 진행중

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
-import Main from './components/main'
-
 const Vue = require('nativescript-vue');
 
+import Main from './components/main.vue'
+
+const Master = {
+  template: `
+    <Main></Main>
+  `,
+  components: Main
+};
+
 new Vue({
-  render: h => h(Main)
+  render: h => h(Master)
 }).$start()
