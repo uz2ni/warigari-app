393852dbdbf7cf92b330147e287da6c0fd65e8b8
uz2ni <uzini_@likelion.org>
2021-05-13 22:19:15 +0900

    [설치] axios, devtool 추가 [axios] 통신 테스트 중 [이슈#4] templateLoader 에러

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
+import VueDevtools from 'nativescript-vue-devtools'
 const Vue = require('nativescript-vue');
 
 import Main from './components/main'
+
+Vue.use(VueDevtools)
 
 new Vue({
   render: h => h("frame", [h(Main)])
--- app/components/tab/home.vue
+++ app/components/tab/home.vue
@@ -1,10 +1,26 @@
 <template>
   <label text="홈, 베스트, 신상"></label>
+  <label v-bind:text="test2"></label>
+  <label v-bind:text="test"></label>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: "home"
+  name: "home",
+  data() {
+    return {
+      test: 'test',
+      test2: 'hello world'
+    }
+  },
+  created() {
+    axios.get('https://it-event-back.herokuapp.com/')
+        .then(response => {
+          this.test = response.data
+        })
+        .catch(error => console.log(error));
+  }
 }
 </script>
 
